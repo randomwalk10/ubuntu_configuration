e2439abee7a8cf91d97c74a66096c31f8fdba218
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-09-16 19:42:54 -0700

    update cuda and driver; house keeping

--- 4_install_cuda_and_driver.sh
+++ 4_install_cuda_and_driver.sh
@@ -8,17 +8,16 @@ sudo service lightdm stop
 #disable nouveau
 sudo cp ./blacklist-nouveau.conf /etc/modprobe.d/
 sudo update-initramfs -u
-#install cuda 8(need to disable opengl)
-sudo sh cuda_8.0.61_375.26_linux.run --override
+#install cuda 10.0(need to disable opengl)
+sudo sh cuda_10.0.130_410.48_linux --override
 #add to path
 cat >> ~/.bashrc << 'EOF'
-export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64\
+export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64\
 ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 EOF
 source ~/.bashrc
 #install patch
-sudo sh cuda_8.0.61.2_linux.run 
 #start gui
 sudo service lightdm start
 #display results
